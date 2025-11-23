ff72f02abd39755a0054379ff0ff153284d8b8ad
Dark Nick <soumyadey983@gmail.com>
2025-11-24 04:45:19 +0530

    Update settings.js for Dark-Nick

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "918145321510", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "Ice~mI6Gzy",
+  OWNER_NUMBER: "+918145321510", 
+  PREFIX: "🥵",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARK GANG USER ( DARK NICK ❤️)
